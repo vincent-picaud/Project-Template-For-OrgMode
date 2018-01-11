ebe5d2270574c47facad75a7ba339599538a9ebf
Pixor <vincent.picaud@cea.fr>
2018-01-11 11:14:05 +0100

    Better name

--- Setup/setupFile.el
+++ Setup/setupFile.el
@@ -1,4 +1,4 @@
-;; [[file:~/GitHub/Project-Template-For-OrgMode/doc/doc.org::*Default%20=setupFile.el=%20file][Default =setupFile.el= file:1]]
+;; [[file:~/GitHub/Project-Template-For-OrgMode/doc/InternalDoc.org::#+BEGIN_SRC%20emacs-lisp%20:tangle%20yes%20:tangle%20"../Setup/setupFile.el"%20(if%20my-project-root%20;;%20Configuration%20(progn%20(message%20"Configuring%20%25s%20"%20my-project-root)%20;;%20;;%20Local%20bibliography%20;;%20(setq%20my-bibtex-filename%20(concat%20my-project-root%20"Bibliography/bibliography.bib"))%20(if%20(file-exists-p%20my-bibtex-filename)%20;;%20If%20bibliography.bib%20exists%20(setq%20reftex-default-bibliography%20`(,my-bibtex-filename)%20bibtex-completion-bibliography%20my-bibtex-filename%20bibtex-completion-library-path%20(file-name-directory%20my-bibtex-filename)%20bibtex-completion-notes-path%20(file-name-directory%20my-bibtex-filename)%20org-ref-default-bibliography%20`(,my-bibtex-filename)%20org-ref-pdf-directory%20(file-name-directory%20my-bibtex-filename)%20)%20;;%20otherwise%20unbound%20meaningless%20my-bibtex-filename%20(makunbound%20'my-bibtex-filename)%20)%20;;%20;;%20Agenda%20files%20;;%20(setq%20org-agenda-files%20(mapcar%20'abbreviate-file-name%20(split-string%20(shell-command-to-string%20(format%20"find%20%25s%20-name%20\"*.org\""%20my-project-root))%20"\n")))%20;;%20;;%20My%20my-workInProgress-filename%20and%20its%20associated%20captures%20;;%20(setq%20my-workInProgress-filename%20(concat%20my-project-root%20"workInProgress.org"))%20(when%20(file-exists-p%20my-workInProgress-filename)%20(setq%20org-capture-templates%20`(%20("t"%20"Todo"%20entry%20(file+headline%20,my-workInProgress-filename%20"Project%20TODO")%20"*%20TODO%20%25^{Title?}%20%5B/%5D%20%25^G\n:PROPERTIES:\n:Created:%20%25U\n:END:\n\n%20-%20%5B%20%5D%20%25?"%20:empty-lines%201%20:create%20t%20)%20("T"%20"Todo%20with%20file%20link"%20entry%20(file+headline%20,my-workInProgress-filename%20"Project%20TODO")%20"*%20TODO%20%25^{Title|%25f}%20%5B/%5D%20%25^G\n:PROPERTIES:\n:Created:%20\%20%25U\n:END:\n\n%5B%5B%25l%5D%5BIn%20file%20%25f%5D%5D:\n\n#+BEGIN_EXAMPLE\n%25i\n#+END_EXAMPLE\n\n%20-%20%5B%20%5D%20%25?"%20:empty-lines%201%20:create%20t%20)%20("j"%20"Journal"%20entry%20(file+olp+datetree%20,my-workInProgress-filename%20"Project%20Journal")%20"*%20%25^{Title}%20%25^G\n\n%25?"%20:empty-lines%201%20:create%20t%20)%20("J"%20"Journal%20with%20file%20link"%20entry%20(file+olp+datetree%20,my-workInProgress-filename%20"Project%20journal")%20"*%20%25^{Title|%25f}%20%25^G\n\n%5B%5B%25l%5D%5BIn%20file%20%25f%5D%5D:\n\n#+BEGIN_EXAMPLE\n%25i\n#+END_EXAMPLE\n\n%25?"%20:empty-lines%201%20:create%20t%20)%20;;%20CAVEAT:%20emacs%20must%20run%20in%20server%20mode%20for%20org-protocol%20;;%20One%20must%20use%20this%20function:%20;;%20;;%20(defun%20transform-square-brackets-to-round-ones(string-to-transform)%20;;%20"Transforms%20%5B%20into%20(%20and%20%5D%20into%20),%20other%20chars%20left%20unchanged."%20;;%20(concat%20;;%20(mapcar%20#'(lambda%20(c)%20(if%20(equal%20c%20?%5B)%20?\(%20(if%20(equal%20c%20?%5D)%20?\)%20c)))%20string-to-transform))%20;;%20)%20;;%20;;%20See:%20https://github.com/sprig/org-capture-extension#set-up-handlers-in-emacs%20;;%20for%20further%20details%20;;%20("L"%20"Protocol%20Link"%20entry%20(file+headline%20,my-workInProgress-filename%20"W3%20Links")%20"*%20%5B%5B%25:link%5D%5B%25(transform-square-brackets-to-round-ones%20\"%25:description\")%5D%5D%20\%20%25^G\n:PROPERTIES:\n:Created:%20%25U\n:END:\n\n%25?"%20:empty-lines%201%20:create%20t%20)%20("p"%20"Protocol"%20entry%20(file+headline%20,my-workInProgress-filename%20"W3%20Links")%20"*%20%5B%5B%25:link%5D%5B%25(transform-square-brackets-to-round-ones%20\"%25:description\")%5D%5D%20\%20%25^G\n:PROPERTIES:\n:Created:%20%25U\n:END:\n#+BEGIN_EXAMPLE\n%25i\n#+END_EXAMPLE\n\n%25?"%20:empty-lines%201%20:create%20t%20)%20)))%20;;%20;;%20How%20to%20publish%20;;%20;;%20Note:%20you%20can%20publish%20somewhere%20else,%20for%20instance:%20;;%20;;%20(setq%20my-publish-dir%20"~/Temp/Publish")%20;;%20;;%20by%20default%20we%20publish%20in-place%20;;%20(advantage:%20C-c%20C-e%20h%20h%20directly%20update%20the%20published%20page)%20(setq%20my-publish-dir%20my-project-root)%20(setq%20my-project-name%20"My_Project_Name")%20(setq%20org-publish-project-alist%20`(%20(,(concat%20my-project-name%20"_Org")%20:base-directory%20,my-project-root%20:base-extension%20"org"%20:recursive%20t%20:publishing-directory%20,my-publish-dir%20:publishing-function%20org-html-publish-to-html%20:htmlize-source%20t%20:org-html-head-include-default-style%20nil%20:exclude%20"Setup*\\|sitemap.org"%20:makeindex%20t%20:auto-sitemap%20t%20:sitemap-title%20,my-project-name%20)%20;;%20("PPack_Tangle"%20;;%20:base-directory%20,my-project-root%20;;%20:base-extension%20"org"%20;;%20:recursive%20t%20;;%20:publishing-directory%20,my-publish-dir%20;;%20:publishing-function%20org-babel-tangle-publish%20;;%20:exclude%20".*bazel-.*"%20;;%20)][Default =setupFile.el= file:1]]
 (if my-project-root
     ;; Configuration
     (progn
@@ -108,7 +108,7 @@
 
       (setq org-publish-project-alist
 	    `(
-	      ("Org_Files"
+	      (,(concat my-project-name "_Org")
 	       :base-directory ,my-project-root
 	       :base-extension "org"
 	       :recursive t
@@ -131,7 +131,7 @@
 	      ;;  :exclude ".*bazel-.*"
 	      ;;  )
 
-	      ("Data_Files"
+	      (,(concat my-project-name "_Data")
 	       :base-directory ,my-project-root
 	       :base-extension "nb\\|?pp\\|png"
 	       :recursive t
@@ -142,9 +142,9 @@
 
 	      ;; Main
 	      (,my-project-name
-	       :components ("Org_Files"
+	       :components (,(concat my-project-name "_Org")
 			    ;;"DefaultProject_Tangle"
-			    "Data_Files")
+			    ,(concat my-project-name "_Data"))
 	       )
 	      )
 	    )
