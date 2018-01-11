3863116aa944056f80b67e2fe2937e5c58963d28
Pixor <vincent.picaud@cea.fr>
2018-01-11 09:50:55 +0100

    Add biblio

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,7 @@
-
-;; [[file:~/GitHub/Project-Template-For-OrgMode/README.org::*The%20%3D.dir-locals.el%3D%20file][The\ =\.dir-locals\.el=\ file:1]]
-
+;; [[file:~/GitHub/Project-Template-For-OrgMode/doc/doc.org::*The%20=.dir-locals.el=%20file][The =.dir-locals.el= file:1]]
 ;; Must exist because this file is used to find my-project-root
 ;;
 ;; Here contains nothing (), however you can fill it with
 ;; your usual stuff.
 ()
-
-;; The\ =\.dir-locals\.el=\ file:1 ends here
+;; The =.dir-locals.el= file:1 ends here
