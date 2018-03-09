e1364c78546910d9f6c17b95519b3e2e15ee6c9b
Picaud Vincent <picaud.vincent@gmail.com>
2018-03-09 12:50:14 +0100

    Remove useless files

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-;; [[file:~/GitHub/Project-Template-For-OrgMode/doc/InternalDoc.org::*The%20=.dir-locals.el=%20file][The =.dir-locals.el= file:1]]
-;; Must exist because this file is used to find my-project-root
-;;
-;; Here contains nothing (), however you can fill it with
-;; your usual stuff.
-()
-;; The =.dir-locals.el= file:1 ends here
