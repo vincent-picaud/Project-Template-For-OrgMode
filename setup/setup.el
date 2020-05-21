1b0d0a8d35bf4b591069993ce9009fa1cf850fdb
Picaud Vincent <picaud.vincent@gmail.com>
2020-05-21 09:47:11 +0200

    remove docs/

--- setup/setup.el
+++ setup/setup.el
@@ -1,1 +1,10 @@
-toto
+(if my-project-root
+    ;; Configuration
+    (progn
+      (setq org-agenda-files
+            (mapcar 'abbreviate-file-name
+      	      (split-string
+      	       (shell-command-to-string (format "find %s -name \"*.org\" ! -name \"index.org\"  ! -name \"agenda.org\"  ! -path \"./setup/*\" ! -path \"./docs/*\"" my-project-root))
+      	       "\n")))
+      )
+  )
